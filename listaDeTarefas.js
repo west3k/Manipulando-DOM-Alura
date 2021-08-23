8e4d7450be40ede6e31749b3b94c657b27072af7
west3k <wesoliveirasilva2@gmail.com>
2021-08-23 12:46:47 -0300

    Criando o item e incluindo na lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -2,12 +2,21 @@ const criarTarefa = (evento) =>{
 
   evento.preventDefault()
 
+  const lista =  document.querySelector('[data-list]')
   const input = document.querySelector('[data-form-input]')
-  console.log(input.value)
+  const valor = input.value
 
+  const tarefa = document.createElement('li')
+  const conteudo = `<p class="content">${valor}</p>`
+
+  tarefa.innerHTML = conteudo
+  tarefa.classList.add('task')
+
+  lista.appendChild(tarefa)
   input.value = " "
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
+
